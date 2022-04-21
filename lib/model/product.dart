cfb3b25998d93183800725a33e00e18c4c7fe775
Dimas Mulya <dimasgans39@gmail.com>
2022-04-21 09:58:34 +0700

    Minggu09

---
+++ lib/model/product.dart
@@ -0,0 +1,9 @@
+class Product{
+  final int id;
+  final String productName;
+  final String productDescription;
+  final String productImage;
+  final double price;
+
+  Product(this.id, this.productName, this.productDescription, this.productImage, this.price);
+}
